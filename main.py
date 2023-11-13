137d511ea1a835d9ddd3a8f6953bb07807e48697
Breno Cupertino <brenonscupertino@gmail.com>
2023-11-13 20:19:49 -0300

    continuando o cenário

--- main.py
+++ main.py
@@ -30,18 +30,25 @@ class Plataforma(BaseImage):
 
 if __name__ == '__main__':
     nivel1: Campo = Campo('./assets/images/campo.jpg', 450, 250)
-    urlMod: str = './assets/images/plataforma.png'
     urlOriginal: str = './assets/images/plataforma-original.png'
     urlRampa: str = './assets/images/rampa.png'
     urlRampa02: str = './assets/images/rampa02.png'
     boy: Personagem = Personagem(115, 450, 'fogo')
-    plataformas: list[Plataforma] = [
+    plataformas: list[list[Plataforma]] = [
         [
-        Plataforma('./assets/images/plataforma01.png', 190, 405), Plataforma(urlOriginal, 815, 405), Plataforma(urlRampa, 776, 405)
+        Plataforma('./assets/images/plataforma01.png', 190, 410), Plataforma(urlOriginal, 815, 410), Plataforma(urlRampa, 776, 410)
         ], 
 
         [
-        Plataforma('./assets/images/plataforma03.png', 227, 310), Plataforma(urlRampa02, 408, 310), Plataforma(urlRampa02, 437, 329),  Plataforma('./assets/images/plataforma02.png', 572, 347), Plataforma(urlRampa02, 710, 347)
+        Plataforma('./assets/images/plataforma03.png', 227, 325), Plataforma(urlRampa02, 408, 325), Plataforma(urlRampa02, 435, 340),  Plataforma('./assets/images/plataforma02.png', 569, 354), Plataforma(urlRampa02, 706, 354), Plataforma(urlOriginal, 85, 310), Plataforma(urlRampa02, 123, 310)
+        ],
+
+        [
+        Plataforma(urlRampa, 180, 230), Plataforma(urlOriginal, 218, 230), Plataforma(urlRampa, 237, 216), Plataforma('./assets/images/plataforma01.png', 379, 216), Plataforma(urlRampa02, 524, 216), Plataforma('./assets/images/plataforma02.png', 657, 230), Plataforma(urlOriginal, 804, 230), Plataforma(urlOriginal, 815, 230)
+        ],
+
+        [
+            Plataforma('./assets/images/plataforma01.png', 300, 135), Plataforma(urlRampa, 430, 120), Plataforma('./assets/images/plataforma04.png', 534, 105), Plataforma(urlRampa02, 639, 105), Plataforma(urlRampa02, 665, 120), Plataforma(urlOriginal, 700, 135), Plataforma(urlOriginal, 748, 135)
         ]
 
         ]
