a20dd8e3ca9a90e69e59b118ea8bb113ccda75ff
MayaraSantos01 <mayararodrigues7887@gmail.com>
2023-11-14 14:00:22 -0300

    mudanças no cenário

---
+++ modules/diamante.py
@@ -0,0 +1,6 @@
+from tupy import *
+
+class Diamante(Image):
+    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None, cor: str | None = None) -> None:
+        super().__init__(file, x,y)
+        self._cor=cor
