a0bc096a8ee9220521d429657a2110431a513085
MayaraSantos01 <mayararodrigues7887@gmail.com>
2023-11-16 20:40:04 -0300

    mudanças no código

--- modules/diamante.py
+++
@@ -1,6 +0,0 @@
-from tupy import *
-
-class Diamante(BaseImage):
-    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None, cor: str | None = None) -> None:
-        super().__init__(file, x,y)
-        self._cor=cor
