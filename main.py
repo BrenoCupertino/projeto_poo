72039b3e22e5844de2b083feab33799fef1af393
Gabriel Baptista de Abreu <38791667+bapbcc@users.noreply.github.com>
2023-11-18 17:09:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from typing import Optional
 from tupy import *
-from modules.personagem import Personagem
+from modules.personagem import Personagem, Porta
 from modules.botao import Botao
 from modules.campo import Campo
-from modules.diamante import Diamante
+from modules.objetos import *
 
-""" class botao1(Botao):
+"""class botao1(Botao):
     def __init__(self,x,y,boy,girl):
         super().__init__(x,y)
         self._boy=boy
@@ -16,8 +16,12 @@ from modules.diamante import Diamante
             self._c1=True
         elif self._collides_with(self._girl):
             self._c1=True 
-        super().update()  """
+        super().update()"""
 
+class Elevador(BaseImage):
+    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None) -> None:
+        super().__init__(file, x, y)
+        
 class Personagem(Personagem):
 
     def update(self):
@@ -29,64 +33,18 @@ class Personagem(Personagem):
         if self._collides_with(elevador):
             self.tempo_caindo = 0
 
-class Plataforma(Image):
-    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None) -> None:
-        super().__init__(file, x, y)
-    
-    def set_x(self, x: int) -> None:
-        self._x = x
-
-    def set_y(self, y: int) -> None:
-        self._y = y
-
-
-class Obstaculo(BaseImage):
-    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None, tipo: str | None = None) -> None:
-        super().__init__(file, x, y)
-        self.tipo=tipo
-
-
-class Porta(BaseImage):
-    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None) -> None:
-        super().__init__(file, x, y)
-
-class Elevador(BaseImage):
-    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None) -> None:
-        super().__init__(file, x, y)
-        
-class Cubo(Image):
-    GRAVIDADE = 2
-
-    def __init__(self, file: str | None = None, x: int | None = None, y: int | None = None) -> None:
-        super().__init__(file, x, y)
-        self.tempo_caindo_cubo = 0
-
-    def update(self):
-        self.y += min(4.9, (self.tempo_caindo_cubo/30)*self.GRAVIDADE)
-        self.tempo_caindo_cubo += 2
-        for lista in plataformas:
-            for item in lista:
-                if self._collides_with(item):
-                    self.tempo_caindo_cubo = 0
-        if self._collides_with(elevador):
-            self.tempo_caindo_cubo = 0
-        if self._collides_with(boy):
-            self.x += boy.velocidade_atual.x
-        if self._collides_with(girl):
-            self.x += girl.velocidade_atual.x
-
 if __name__ == '__main__':
     nivel1: Campo = Campo('./assets/images/campo-teste.png', 450, 250)
     urlOriginal: str = './assets/images/plataforma-original.png'
     urlRampa: str = './assets/images/rampa.png'
     urlRampa02: str = './assets/images/rampa02.png'
-    boy: Personagem = Personagem(115, 439, 'fogo')
-    girl: Personagem = Personagem(115, 360, 'agua')
     porta0: Porta = Porta("./assets/images/firegate0.png", 110, 92)
     porta1: Porta = Porta("./assets/images/watergate0.png", 210, 92)
+    boy: Personagem = Personagem(115, 439, 'fogo', porta1)
+    girl: Personagem = Personagem(115, 360, 'agua', porta0)
     elevador: Elevador = Elevador("./assets/images/elevador.png",800, 230)
     botao0: Botao = Botao("./assets/images/botao.png",730,219)
-    botao1: Botao = Botao("./assets/images/botao.png",700,125)
+    botao2: Botao = Botao("./assets/images/botao.png",700,125)
     cubo: Cubo = Cubo('./assets/images/cubo.png',200,300)
     plataformas: list[list[Plataforma]] = [
         [
@@ -111,4 +69,5 @@ if __name__ == '__main__':
     poison: Obstaculo = Obstaculo('./assets/images/poison0.png', 570, 362)
     fire: Obstaculo = Obstaculo('./assets/images/fire0.png', 397, 481)
     water: Obstaculo = Obstaculo('./assets/images/water0.png', 592, 481)
+
     run(globals())
